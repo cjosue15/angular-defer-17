45efaac5b94c686351353e2305402ac9ee70c322
Josue WSL2 <dev.carlos.morales@gmail.com>
2023-10-21 12:41:19 -0500

    defer when imperative condition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 
-import { HomeComponent } from './pages/home/home.component';
+import { HomeComponent, DeferWhenComponent } from './pages/home';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [HomeComponent],
+  imports: [HomeComponent, DeferWhenComponent],
 })
 export class AppComponent {
   title = 'angular-defer-v17';
